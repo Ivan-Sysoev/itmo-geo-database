957281fe828ad8c08e08d5071e65a92d11aa3a8f
Ivan-Sysoev <ivansysoev007@gmail.com>
2025-12-24 20:06:52 +0300

    Экспортировал в csv

---
+++ src/parser/export_csv.py
@@ -0,0 +1,32 @@
+import sqlite3
+import csv
+
+# Подключение к базе
+conn = sqlite3.connect("../geo_analysis.db")
+cursor = conn.cursor()
+
+# --- Экспорт таблицы bakeries ---
+cursor.execute("SELECT * FROM bakeries")
+rows = cursor.fetchall()
+columns = [description[0] for description in cursor.description]
+
+with open("bakeries.csv", "w", newline="", encoding="utf-8") as f:
+    writer = csv.writer(f)
+    writer.writerow(columns)  # заголовки
+    writer.writerows(rows)
+
+print("Таблица bakeries экспортирована в bakeries.csv")
+
+# --- Экспорт таблицы infrastructure ---
+cursor.execute("SELECT * FROM infrastructure")
+rows = cursor.fetchall()
+columns = [description[0] for description in cursor.description]
+
+with open("infrastructure.csv", "w", newline="", encoding="utf-8") as f:
+    writer = csv.writer(f)
+    writer.writerow(columns)  # заголовки
+    writer.writerows(rows)
+
+print("Таблица infrastructure экспортирована в infrastructure.csv")
+
+conn.close()
